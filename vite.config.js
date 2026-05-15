a8169920abe17f834d4d020dcfc861798c861fa8
吴俊龙 <1624679067@qq.com>
2026-05-15 10:49:09 +0800

    保存 txt文件到根目录

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { readFileSync } from 'node:fs'
+
+const keepRootTxtFile = () => ({
+  name: 'keep-root-txt-file',
+  apply: 'build',
+  generateBundle() {
+    this.emitFile({
+      type: 'asset',
+      fileName: '1d7410b885c7b9323fb2d282d6f2bf71.txt',
+      source: readFileSync('1d7410b885c7b9323fb2d282d6f2bf71.txt')
+    })
+  }
+})
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), keepRootTxtFile()],
   server: {
     host: true,
     port: 5173
